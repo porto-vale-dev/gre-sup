e3820786961b19692b7987e07937e2af4460229f
Porto Vale <portovale.ad@gmail.com>
2025-06-30 15:07:49 +0000

    Correção de url:
    O sistema está carregando em /suporte-gre/
    Substitua o

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  basePath: '/suporte-gre',
   output: 'standalone', // Essencial para deploy com Docker/Cloud Run
   allowedDevOrigins: [
  'https://9000-firebase-studio-1749749009211.cluster-m7tpz3bmgjgoqrktlvd4ykrc2m.cloudworkstations.dev',
