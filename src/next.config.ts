dfde3edc0daad6704b307f32af1008760b2c7fa9
Porto Vale <portovale.ad@gmail.com>
2025-07-02 19:42:47 +0000

    está retornando esse erro no commit do github �[91m ⚠ Invalid next.confi

---
+++ src/next.config.ts
@@ -0,0 +1,39 @@
+
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  output: 'standalone', // Essencial para deploy com Docker/Cloud Run
+  async rewrites() {
+    return [
+      {
+        source: '/suporte-gre/painel',
+        destination: '/dashboard',
+      },
+      {
+        source: '/suporte-gre/painel/archived',
+        destination: '/dashboard/archived',
+      },
+    ];
+  },
+  typescript: {
+    // Forçar a verificação de tipos no build para garantir a qualidade do código
+    ignoreBuildErrors: false,
+  },
+  eslint: {
+    // Forçar a verificação do linter no build
+    ignoreDuringBuilds: false,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
