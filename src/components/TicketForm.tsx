b59ec313c14d9e3733eb0cc8a74e453d73021a43
Porto Vale <portovale.ad@gmail.com>
2025-09-03 20:31:51 +0000

    deixa esse texo embaixo do atenção em vermelho

--- src/components/TicketForm.tsx
+++ src/components/TicketForm.tsx
@@ -202,14 +202,14 @@ export function TicketForm() {
             </div>
           </div>
         </div>
+        <div className="flex items-center gap-2 text-xs text-red-600 font-semibold p-2 rounded-md bg-red-100 border border-red-200 mt-4">
+                <CalendarClock className="h-4 w-4"/>
+                <span>Os prazos dos tickets enviados agora serão contados a partir de 08/09 (segunda-feira).</span>
+            </div>
         <div className="flex justify-between items-center pt-2">
             <CardDescription>
                 Preencha o formulário abaixo para registrar seu chamado.
             </CardDescription>
-            <div className="flex items-center gap-2 text-xs text-orange-600 font-semibold p-2 rounded-md bg-orange-100 border border-orange-200">
-                <CalendarClock className="h-4 w-4"/>
-                <span>Os prazos dos tickets enviados agora serão contados a partir de 08/07 (segunda-feira).</span>
-            </div>
         </div>
       </CardHeader>
       <CardContent>
